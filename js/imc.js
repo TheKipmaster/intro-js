e73f6aced16fc12bb945b8e4697a5c3ed0b08a33
TheKipmaster <fkipman@hotmail.com>
2017-07-26 01:50:03 -0300

    Initial commit (aula 5 do curso introdutório de JS do alura)

---
+++ js/imc.js
@@ -0,0 +1,39 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = pacientes[i].querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = pacientes[i].querySelector(".info-imc");
+
+  var alturaEhValida = true;
+  var pesoEhValido = true;
+
+  if(peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido";
+    pesoEhValido = false;
+  }
+
+  if(altura <= 0 || altura >= 3) {
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida";
+    alturaEhValida = false;
+  }
+
+  if(!alturaEhValida || !pesoEhValido) {
+    pacientes[i].classList.add("paciente-invalido");
+  }
+
+  if(alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(3);
+  }
+}
