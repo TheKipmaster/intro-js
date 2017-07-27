4dcd7f9c1af5cc08afbd92d9730acfcf3e96ddb0
TheKipmaster <fkipman@hotmail.com>
2017-07-26 21:08:53 -0300

    Refatora validação de altura e peso

--- js/imc.js
+++ js/imc.js
@@ -17,27 +17,36 @@ for (var i = 0; i < pacientes.length; i++) {
 
   var tdImc = pacientes[i].querySelector(".info-imc");
 
-  var alturaEhValida = true;
-  var pesoEhValido = true;
+  var alturaEhValida = validaAltura(altura);
+  var pesoEhValido = validaPeso(peso);
 
-  if(peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido";
-    pesoEhValido = false;
+  if(pesoEhValido && alturaEhValida) {
+    tdImc.textContent = calculaImc(peso, altura);
   }
-
-  if(altura <= 0 || altura >= 3) {
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida";
-    alturaEhValida = false;
-  }
-
-  if(!alturaEhValida || !pesoEhValido) {
+  else {
     pacientes[i].classList.add("paciente-invalido");
+    if(!pesoEhValido) {
+      console.log("Peso inválido");
+      tdImc.textContent = "Peso inválido";
+    } else {
+      console.log("Altura inválida");
+      tdImc.textContent = "Altura inválida";
+    }
   }
+}
 
-  if(alturaEhValida && pesoEhValido) {
-    var imc = calculaImc(peso, altura);
-    tdImc.textContent = imc;
+function validaPeso(peso) {
+  if(peso > 0 && peso < 1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if(altura > 0 && altura < 3) {
+    return true;
+  } else {
+    return false;
   }
 }
